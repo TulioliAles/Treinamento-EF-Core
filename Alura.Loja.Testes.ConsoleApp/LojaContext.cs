e8bb61748d20055a0c55a7163581628e4ba58620
Alessandro Tulioli <alessandro.tulioli@sp01.local>
2021-12-20 14:17:13 -0300

    Criados métodos de Gravar e Recuperar dados

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=RBPN310100110\\SQLEXPRESS;Database=LojaDB;Trusted_Connection=true;");
+        }
+    }
+}
